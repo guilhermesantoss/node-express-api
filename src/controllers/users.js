72fbc2bb1a91864da13a4c28ce6b40601c740dfb
Guilherme Lopes dos Santos <g.santos.sfc@gmail.com>
2021-02-03 19:56:53 -0300

    add demais metodos

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,9 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 
-let users = [{
-  "name": "Fernando",
-  "age": 29
-}];
+let users = [];
 
 // export const getUsers = (req, res) => {
 export function getUsers(req, res) {
@@ -12,15 +9,8 @@ export function getUsers(req, res) {
 
 export const createUsers = (req, res) => {
   const user = req.body;
-  
-  const userWithId = { id: uuidv4(), ...user };
 
-  // userWithId = {
-  //   id: "62b8f76c-b1e9-43ed-b78f-fcb94e17850a",
-  //   firstName: "Margareth",
-  //   lastName: "Sulivan",
-  //   age: 35
-  // }
+  const userWithId = { id: uuidv4(), ...user };
 
   users.push(userWithId);
  
@@ -33,38 +23,38 @@ export const createUsers = (req, res) => {
   // res.status(201).send(`User with the name ${user.firstName} added to the database!`);
 };
 
-// export const getUser = (req, res) => {
-//   const { id } = req.params;
+export const getUser = (req, res) => {
+  const { id } = req.params;
   
-//   const foundUser = users.find((user) => user.id === id);
+  const foundUser = users.find((user) => user.id === id);
 
-//   if (!foundUser) {
-//     res.json({
-//       status: 204,
-//       message: "Invalid ID"
-//     });
-//   } else {
-//     res.send(foundUser);
-//   }  
-// };
+  if (!foundUser) {
+    res.json({
+      status: 204,
+      message: "Invalid ID"
+    });
+  } else {
+    res.send(foundUser);
+  }  
+};
 
-// export const deleteUser = (req, res) => {
-//   const { id } = req.params;
+export const deleteUser = (req, res) => {
+  const { id } = req.params;
 
-//   users = users.filter((user) => user.id !== id);
+  users = users.filter((user) => user.id !== id);
 
-//   res.send(`User with the id ${id} deleted from the database.`);
-// };
+  res.send(`User with the id ${id} deleted from the database.`);
+};
 
-// export const updateUser = (req, res) => {
-//   const { id } = req.params;
-//   const { firstName, lastName, age } = req.body;
+export const updateUser = (req, res) => {
+  const { id } = req.params;
+  const { firstName, lastName, age } = req.body;
 
-//   const user = users.find((user) => user.id  === id);
+  const user = users.find((user) => user.id  === id);
 
-//   if (firstName) user.firstName = firstName;
-//   if (lastName) user.lastName = lastName;
-//   if (age) user.age = age;
+  if (firstName) user.firstName = firstName;
+  if (lastName) user.lastName = lastName;
+  if (age) user.age = age;
   
-//   res.send(`User with the id ${id} has been updated`);
-// };
+  res.send(`User with the id ${id} has been updated`);
+};
